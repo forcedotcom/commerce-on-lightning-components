4d2c93b62605ff470ee423172cf6ef1eca09aacc
Syed Shehroz Hussain [Ozzy] <s.hussain@salesforce.com>
2025-08-05 11:41:28 -0400

    fix: missing config files

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,18 @@ setupFilesAfterEnv.push('<rootDir>/jest-sa11y-setup.js');
 module.exports = {
     ...jestConfig,
     passWithNoTests: true,
-    moduleNameMapper: {},
+    moduleNameMapper: {
+        '^lightning/modal$': '<rootDir>/force-app/test/jest-mocks/lightning/modal/modal.js',
+    },
     setupFilesAfterEnv,
+    preset: '@lwc/jest-preset',
+    moduleFileExtensions: ['js', 'html'],
+    coverageThreshold: {
+        global: {
+            branches: 88,
+            functions: 91,
+            lines: 93,
+            statements: 93,
+        },
+    },
 };
