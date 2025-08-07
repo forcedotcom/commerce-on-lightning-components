dc6c603b685df80373455acecee22e3a21e5bf2c
avinashkasipathy <akasipathy@salesforce.com>
2025-08-07 10:48:29 -0700

    Initial commit deleting the existing release branch
    Cleaned up dotfiles and config directories from miaw-messaging-release branch

--- commitlint.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'scope-case': [2, 'always', 'lower-case'],
-        'scope-enum': [2, 'always', ['component', 'release', 'deps']],
-        'subject-exclamation-mark': [2, 'never'],
-    },
-};
